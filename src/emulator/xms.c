9cf5597912f65186b444e3ab3cd9f9db5f4ffe58
xrip <xrip@xrip.ru>
2024-09-24 22:05:10 +0300

    adlib added

--- src/emulator/xms.c
+++ src/emulator/xms.c
@@ -1,7 +1,7 @@
 #include "emulator.h"
 
 uint8_t xms_handler() {
-    printf("XMS: %04X:%04X\n", CPU_AH, CPU_DX);
+//    printf("XMS: %04X:%04X\n", CPU_AH, CPU_DX);
     switch (CPU_AH) {
         case 0x00: { // Get XMS Version
             CPU_AX = 0x0200;
@@ -16,17 +16,15 @@ uint8_t xms_handler() {
         }
         case 0x02: { // Release HMA
             // Stub: Implement HMA release functionality
-            CPU_AX = 0; // Success
+            CPU_AX = 1; // Success
             break;
         }
         case 0x03: { // Global Enable A20
             // Stub: Implement A20 enabling functionality
-            CPU_AX = 0; // Success
             break;
         }
         case 0x04: { // Global Disable A20
             // Stub: Implement A20 disabling functionality
-            CPU_AX = 0; // Success
             break;
         }
         case 0x05: { // Local Enable A20
@@ -42,51 +40,38 @@ uint8_t xms_handler() {
         }
         case 0x07: { // Query free extended memory
             // Stub: Implement querying free extended memory functionality
-            CPU_AX = 0x0000; // Least significant word (free memory in KB)
             break;
         }
         case 0x08: { // Query largest free extended memory block
             // Stub: Implement querying largest free extended memory block functionality
-            CPU_DX = 0x0000; // Most significant word (size of largest block in KB)
-            CPU_AX = 0x0000; // Least significant word (size of largest block in KB)
             break;
         }
         case 0x09: { // Allocate extended memory block
             // Stub: Implement allocating extended memory block functionality
-            CPU_DX = 0xFFFF; // Handle of allocated memory block
-            CPU_AX = 0; // Success
             break;
         }
         case 0x0A: { // Free extended memory block
             // Stub: Implement freeing extended memory block functionality
-            CPU_AX = 0; // Success
             break;
         }
         case 0x0B: { // Move extended memory block
             // Stub: Implement moving extended memory block functionality
-            CPU_AX = 0; // Success
             break;
         }
         case 0x0C: { // Lock extended memory block
             // Stub: Implement locking extended memory block functionality4
-            CPU_AX = 0; // Success
             break;
         }
         case 0x0D: { // Unlock extended memory block
             // Stub: Implement unlocking extended memory block functionality
-            CPU_AX = 0; // Success
             break;
         }
-        case 0x0E: { // Query XMS driver capabilities
+        case 0x0E: { // Get EMB Handle Information (Function 0Eh):
             // Stub: Implement querying XMS driver capabilities functionality
-            CPU_DX = 0x0000; // Capabilities
-            CPU_AX = 0; // Success
             break;
         }
-        case 0x0F: { // Query any free extended memory block
+        case 0x0F: { // Reallocate Extended Memory Block (Function 0Fh):
             // Stub: Implement querying any free extended memory block functionality
-            CPU_DX = 0x0000; // Most significant word (size of any free block in KB)
-            CPU_AX = 0x0000; // Least significant word (size of any free block in KB)
             break;
         }
         case 0x10: { // Request Upper Memory Block (Function 10h):
@@ -114,7 +99,7 @@ uint8_t xms_handler() {
         case 0x11: { // Query DMA buffer information
             // Stub: Implement querying DMA buffer information functionality
             CPU_DX = 0x0000; // Most significant word (DMA buffer size)
-            CPU_AX = 0x0000; // Least significant word (DMA buffer size)
+            CPU_AX = 0x0001; // Least significant word (DMA buffer size)
             break;
         }
         default: {
