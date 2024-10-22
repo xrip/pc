aecd8e2ff05eec5f3b5239f4038858a472ce92f9
xrip <xrip@xrip.ru>
2024-10-22 11:26:01 +0300

    hwsound

--- src/emulator/xms.c
+++ src/emulator/xms.c
@@ -13,6 +13,14 @@ typedef struct umb {
     bool allocated;
 } umb_t;
 
+typedef struct  {
+    uint32_t length;
+    uint16_t source_handle;
+    uint32_t source_offset;
+    uint16_t destination_handle;
+    uint32_t destination_offset;
+} __attribute__((packed)) move_data_t;
+
 static umb_t umb_blocks[] = {
 // Used by EMS driver
 //        { 0xC000, 0x0800, false },
@@ -41,18 +49,44 @@ umb_t *get_free_umb_block(uint16_t size) {
 #define XMS_VERSION 0x00
 #define REQUEST_HMA 0x01
 #define RELEASE_HMA 0x02
-#define ENABLE_A20 0x05
-#define DISABLE_A20 0x06
+#define GLOBAL_ENABLE_A20 0x03
+#define GLOBAL_DISABLE_A20 0x04
+#define LOCAL_ENABLE_A20 0x05
+#define LOCAL_DISABLE_A20 0x06
 #define QUERY_A20 0x07
+
+#define QUERY_EMB 0x08
 #define ALLOCATE_EMB 0x09
+#define RELEASE_EMB 0x0A
+#define MOVE_EMB 0x0B
+#define LOCK_EMB 0x0C
+#define UNLOCK_EMB 0x0D
+#define EMB_HANDLE_INFO 0x0E
+#define REALLOCATE_EMB 0x0F
+
 #define REQUEST_UMB 0x10
 #define RELEASE_UMB 0x11
 
+#define XMS_SIZE (1024 << 10)
+#define XMS_HANDLES
+uint8_t XMS[XMS_SIZE * 4] = { 0 };
+uint32_t xms_available = XMS_SIZE;
+
+struct {
+    uint8_t handle;
+    uint32_t size; //
+    uint8_t locks;
+} XMS_HANDLE[XMS_HANDLES] = { 0 };
+uint8_t xms_handles = 0;
+
+int a20_enabled = 0;
+
 uint8_t xms_handler() {
-//    printf("[XMS] %02X\n", CPU_AH);
+    if (CPU_AH > 0x7 && CPU_AH < 0x10)
+    printf("[XMS] %02X\n", CPU_AH);
     switch (CPU_AH) {
         case XMS_VERSION: { // Get XMS Version
-            CPU_AX = 0x0400;
+            CPU_AX = 0x0200;
             CPU_BX = 0x0001; // driver version
             CPU_DX = 0x0001; // HMA Exist
             break;
@@ -67,25 +101,57 @@ uint8_t xms_handler() {
             CPU_AX = 1; // Success
             break;
         }
-        case ENABLE_A20: { // Local Enable A20
-            CPU_AX = 0x0001; // Success
-            CPU_BL = 0x00;
+        case GLOBAL_ENABLE_A20:
+        case LOCAL_ENABLE_A20: { // Local Enable A20
+            CPU_AX = 1; // Success
+            CPU_BL = 0;
+            a20_enabled = 1;
             break;
         }
-        case DISABLE_A20: { // Local Disable A20
+        case GLOBAL_DISABLE_A20:
+        case LOCAL_DISABLE_A20: { // Local Disable A20
             CPU_AX = 1; // Failed
             CPU_BL = 0;
+            a20_enabled = 0;
             break;
         }
         case QUERY_A20: { // Query A20 (Function 07h):
             CPU_AX = 1; // Success
             break;
         }
+
+        case QUERY_EMB : { // 08h
+            printf("[XMS] Query free\r\n");
+            CPU_AX = XMS_SIZE >> 10;
+            CPU_DX = 64;
+            CPU_BL = 0;
+            break;
+        }
         case ALLOCATE_EMB: { // Allocate Extended Memory Block (Function 09h):
+            printf("[XMS] Allocate %dKb\n", CPU_DX);
             // Stub: Implement allocating extended memory block functionality
-            CPU_DX = 0x0000;
-            CPU_AX = 0x0000;
-            CPU_BL = 0xA0;
+            CPU_DX = 0xBEEF;
+            CPU_AX = 1;
+            CPU_BL = 0;
+            break;
+        }
+
+        case MOVE_EMB: {
+                uint32_t struct_offset = ((uint32_t) CPU_DS << 4) + CPU_SI;
+                move_data_t move_data = { 0 };
+                uint8_t * move_data_ptr = (uint8_t *)&move_data;
+                for (int i = 0; i < sizeof(move_data_t); i++) {
+                    *move_data_ptr++ = readw86(struct_offset++);
+                }
+
+            printf("[XMS] Move EMB 0x%06X\r\n\t length 0x%08X \r\n\t src_handle 0x%04X \r\n\t src_offset 0x%08X \r\n\t dest_handle 0x%04X \r\n\t dest_offset 0x%08X \r\n",
+                   struct_offset,
+                   move_data.length,
+                   move_data.source_handle,
+                   move_data.source_offset,
+                   move_data.destination_handle,
+                   move_data.destination_offset
+            );
             break;
         }
         case REQUEST_UMB: { // Request Upper Memory Block (Function 10h):
@@ -117,7 +183,6 @@ uint8_t xms_handler() {
             CPU_BL = umb_blocks_allocated >= UMB_BLOCKS_COUNT ? 0xB1 : 0xB0;
             break;
         }
-
         case RELEASE_UMB: { // Release Upper Memory Block (Function 11h)
             // Stub: Release Upper Memory Block
             for (int i = 0; i < UMB_BLOCKS_COUNT; i++)
